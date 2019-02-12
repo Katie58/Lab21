cee1fe935fc733ca49a8581b70234ff0c8361d0c
katie <kateann58@gmail.com>
2019-02-12 08:58:27 -0500

    first commit

---
+++ WebContent/testing.js
@@ -0,0 +1,9 @@
+function testing1() {
+	return "public class HangmanApp {";
+}
+function testing2() {
+	return "   public static void main(String[] args) {";
+}
+function testing3() {
+	return "      HangManUtil.greeting();";
+}
